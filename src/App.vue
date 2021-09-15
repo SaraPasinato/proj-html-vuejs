55137c9acc8b4eba43a63d1ed8902b4310e9988d
SaraPasinato <pasinatosara.sp@gmail.com>
2021-09-15 14:01:51 +0200

    add comments

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,15 @@
 <template>
-  <div id="app">
+  <div id="app" class="container">
    <!-- Header component contai--->
 
    <!-- Main--->
    <main>
      <!--  Jumbotron component-->
-     <!-- section consultants component-->
-     <!--  section about us component-->
-     <!-- section project component-->
-     <!-- section testimonial component-->
-     <!-- section blog component -->
+     <!-- section consultants component ( img + card component )-->
+     <!--  section about us component  -->
+     <!-- section project component (figure card component)-->
+     <!-- section testimonial component ( testimonial card component) -->
+     <!-- section blog component  (figure card component + (date + card component)-->
      <!--section lets talk component -->
    </main>
    <!-- Footer (contains links Footer + TermsFooter)--->

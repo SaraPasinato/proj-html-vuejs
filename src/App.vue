30570812dfa7f8136ed18a9081bef9db0c2ec1ae
SaraPasinato <pasinatosara.sp@gmail.com>
2021-09-15 15:06:13 +0200

    add header

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,12 @@
 <template>
   <div id="app" class="container">
-   <!-- Header component contai--->
-
-   <!-- Main--->
+   <!--TODO >>now<< Header component contai--->
+   <Header :links="links"/>
+   <!-- main--->
    <main>
      <!--  Jumbotron component-->
      <!-- section consultants component ( img + card component )-->
-     <!--  section about us component  -->
+     <!-- section about us component  -->
      <!-- section project component (figure card component)-->
      <!-- section testimonial component ( testimonial card component) -->
      <!-- section blog component  (figure card component + (date + card component)-->
@@ -17,16 +17,59 @@
 </template>
 
 <script>
-
+import Header from './components/Header.vue';
 
 export default {
   name: 'App',
   components: {
-   
-  }
+    Header,
+  },
+  data(){
+    return{
+      links:[
+        {
+          title:'home',
+          link:'#',
+          current:true,
+        },
+        {
+          title:'about',
+          link:'#',
+          current:false,
+
+        },
+        {
+          title:'product',
+          link:'#',
+          current:false,
+          
+        },
+        {
+          title:'services',
+          link:'#',
+          current:false,
+
+        },
+        {
+          title:'blog',
+          link:'#',
+          current:false,
+
+        },
+        {
+          title:'contact',
+          link:'#',
+          current:false,
+
+        },
+      ],
+    }
+  },
 }
 </script>
 
 <style lang="scss">
 @import '/assets/scss/style.scss';
+
+
 </style>
---
+++ src/components/Header.vue
@@ -0,0 +1,44 @@
+<template>
+  <header>
+    <div class="row">
+      <img src="../assets/images/logotype.png" alt="logo phlox" />
+
+      <ul class="row">
+        <li
+          v-for="(link, index) in links"
+          :key="link.title + index"
+          :class="{ active: link.current }"
+        >
+          <a :href="link.link">{{ link.title }}</a>
+        </li>
+      </ul>
+    </div>
+  </header>
+</template>
+
+<script>
+export default {
+  name: "Header",
+  props: ["links"],
+  //TODO: set active on click 
+};
+</script>
+
+<style lang="scss">
+header {
+  height: 20vh;
+  border: 2px solid white;
+  background-color: transparent;
+
+  .row {
+    height: 100%;
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+  }
+
+  img {
+    width: 7%;
+  }
+}
+</style>

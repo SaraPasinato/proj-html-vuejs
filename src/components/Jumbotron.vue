c2a9e7adf127ed5704eaadfc35707cc5c1567950
SaraPasinato <pasinatosara.sp@gmail.com>
2021-09-16 11:02:38 +0200

    add jubotron

--- src/components/Jumbotron.vue
+++ src/components/Jumbotron.vue
@@ -3,7 +3,14 @@
    <div class="row">
        <div class="col-6">
            <div class="items">
-               
+               <div class="line-title">
+                   <div class="line"/>
+                   <h5> always te best way you need it</h5>
+               </div>
+               <h1> the best business consulting</h1>
+               <div class="btn-control">
+                   <Button type="btn-green" title="Read more"/>
+               </div>
            </div>
        </div>
        <div class="col jumbo-bg"></div>
@@ -12,8 +19,12 @@
 </template>
 
 <script>
+import Button from './Button.vue';
 export default {
  name:"Jumbotron",
+ components:{
+     Button,
+ }
 }
 </script>
 
@@ -37,20 +48,39 @@ export default {
 
             .items{
                 position: absolute;
-                width:100%;
+                width:80%;
                 height: 50%;
-                background-color: palegoldenrod;
                 transform: translate(-50%,-50%);
                 top:50%;
-                left:70%;
+                left:80%;
+
+
+                h5{
+                    font-size: .9em;
+                    letter-spacing: 0.2em;
+                    color:$t-gray-1;
+                }
+                .btn-control,
+                .line-title{
+                    display: flex;
+                    align-items: center;
+                }
+               .line{
+                  background-color:$t-green;
+                  width:5%;
+                  height: 3px;
+                  margin-right: 10px;
+               }
+
+             
             }
 
         }
        
         .jumbo-bg{
             background-image:url('../assets/images/portrait-of-young-architect-woman-on-meeting-KFZCE3A.jpg');
-            background-position-x: left;
-            background-size: 100%;
+            background-position: top ;
+            background-size: cover;
             background-repeat: no-repeat;
             
         }

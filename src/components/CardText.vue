dad0215df40fb553a8b92148c68d147f3f314321
SaraPasinato <pasinatosara.sp@gmail.com>
2021-09-17 15:34:51 +0200

    add car client hover

--- src/components/CardText.vue
+++ src/components/CardText.vue
@@ -3,8 +3,10 @@
     <div class="items">
       <blockquote>{{ card.desc }}</blockquote>
       <cite>{{ card.author }}</cite>
-      <small>{{ card.team }}</small>
-      
+      <small>{{ card.team }}</small>  
+    </div>
+    <div class="laydown">
+      <img :src="getImage" :alt="card.author">
     </div>
   </div>
 </template>
@@ -13,6 +15,11 @@
 export default {
   name: "CardText",
   props: ["card"],
+  computed: {
+    getImage() {
+      return require("../assets/images/" + this.card.url);
+    },
+  },
 };
 </script>
 
@@ -20,10 +27,19 @@ export default {
 @import "../assets/scss/vars";
 
 .card {
-  flex-basis: calc(100% / 4);
-  height: 70%;
-  margin: 0 20px;
+  position: relative;
+  flex-basis: 23%;
+  height: 82%;
+ // margin: 0 20px;
+
   .items {
+    position: absolute;
+    top: 20%;
+    left: 0;
+    z-index: 2;
+
+    width: 100%;
+    height: 100%;
     padding: 0 15%;
 
     blockquote{
@@ -40,5 +56,36 @@ export default {
        color: $t-gray-7;
     }
   }
+
+  .laydown{
+    display: none;
+    position: absolute;
+    top: 0;
+    left: 0;
+    width: 100%;
+    height: 100%;
+
+    img{
+      width: 100%;
+      height: 100%;
+      opacity: 0.9;
+    }
+  }
+}
+
+.card:hover .laydown {
+  display: block;
+}
+
+.card:hover  small,
+.card:hover  blockquote{
+   color: $t-gray-1;
+   font-weight: normal;
+   line-height: 20px;
+}
+.card:hover cite{
+   color: $t-light;
+   font-weight: bolder;
+   font-size: 20px;
 }
 </style>
--- src/components/TestimonialSection.vue
+++ src/components/TestimonialSection.vue
@@ -1,77 +1,76 @@
 <template>
-  <section id="testimonial" >
- <div id="title" class="row container-sm">
+  <section id="testimonial">
+    <div id="title" class="row container-sm">
       <div class="col-12">
         <div class="line-item">
           <h5>Testimonial</h5>
           <div class="line" />
         </div>
         <div class="line-item">
-          <h3>What's the customer say <br/>about our work</h3>
-
+          <h3>What's the customer say <br />about our work</h3>
         </div>
       </div>
     </div>
- <div id="gallery" class="row container">
-        <CardText v-for="(card,index) in cards" :key="index" :card="card" />      
- </div>
+    <div id="gallery" class="row container">
+      <CardText v-for="(card, index) in cards" :key="index" :card="card" />
+    </div>
   </section>
 </template>
 
 <script>
-import CardText from '../components/CardText.vue';
+import CardText from "../components/CardText.vue";
 export default {
-    name:"TestimonialSection",
-    props:['cards'],
-    components:{
-        CardText,
-    }
-}
+  name: "TestimonialSection",
+  props: ["cards"],
+  components: {
+    CardText,
+  },
+};
 </script>
 
 <style lang="scss">
-@import '../assets/scss/vars';
+@import "../assets/scss/vars";
 
-#testimonial{
-    height: 50vh;
-    margin-bottom: 80px;
+#testimonial {
+  height: 50vh;
+  margin-bottom: 80px;
 
-    #title {
-        height: 30%;
-        .line-item {
-        display: flex;
-        align-items: center;
-        justify-content: space-between;
-        text-align: right;
-        h5 {
-          font-weight: normal;
-          font-size: 0.9em;
-          letter-spacing: 0.2em;
-          color: $t-gray-1;
-        }
-
-        .line {
-          background-color: $bg-yellow;
-          width: 7%;
-          height: 2px;
-          margin-right: 1px;
-        }
+  #title {
+    height: 30%;
+    .line-item {
+      display: flex;
+      align-items: center;
+      justify-content: space-between;
+      text-align: right;
+      h5 {
+        font-weight: normal;
+        font-size: 0.9em;
+        letter-spacing: 0.2em;
+        color: $t-gray-1;
       }
 
+      .line {
+        background-color: $bg-yellow;
+        width: 7%;
+        height: 2px;
+        margin-right: 1px;
+      }
     }
+  }
 
-    #gallery{
-        height: 70%;
-        margin-left:20%;
-        margin-top: 40px;
-        width:80%;
-        justify-content: flex-end;
-        align-items: center;
-        background-color: $bg-light;
-    }
-    .row {
+  #gallery {
+    height: 70%;
+    margin-left: 20%;
+    margin-top: 40px;
+    width: 80%;
+    justify-content: flex-end;
+    align-items: center;
+    background-color: $bg-light;
+    padding-left: 10px;
+  }
+  .row {
     display: flex;
     flex-direction: row-reverse;
-    }
+  }
 }
 </style>
